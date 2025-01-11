6b42e92fa740b49b8ed96784848d840478d73098
Evgeniy <evgnec@gmail.com>
2025-01-11 20:11:32 +0200

    upd

---
+++ src/pr01/js/cat.js
@@ -0,0 +1,8 @@
+const refs = {
+  BASE_URL: 'https://api.thecatapi.com/v1',
+  URL : `https://api.thecatapi.com/v1/images/search?limit=20`,
+  ENDPOINT: '/breeds',
+  KEY: 'live_6mSRucqNyrSNqpDecLOPuWFbMXAyH4nVJnDAVE1MvYAWkJLHdyznA0qqn2NkF7EW',
+  list: document.querySelector('.js-list'),
+  btnLoadMore: document.querySelector('.js-load-more'),
+};
