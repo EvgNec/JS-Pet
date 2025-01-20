6ef02c195713d2d7a625464742647b0579a9c101
Evgeniy <evgnec@gmail.com>
2025-01-20 20:29:56 +0200

    add post

--- src/pr03/js/fetch.js
+++ src/pr03/js/fetch.js
@@ -1,5 +1,8 @@
 const API = 'https://jsonplaceholder.typicode.com/posts';
 const cards = document.querySelector('.cards');
+const inputTitle = document.querySelector('.input-title');
+const inputDescription = document.querySelector('.input-description');
+const addButton = document.querySelector('.btn-add')
 let newCard = '';
 
 function getPosts() {
@@ -30,7 +33,11 @@ cards.addEventListener('click', e => {
   if (e.target.classList.contains('btn-delete')) {
     const id = e.target.getAttribute('data-id');
     deletePost(id, e.target.closest('.card'));
-  }
+    }
+     if (e.target.classList.contains('btn-add')) {
+         addPost();
+    } 
+
 });
 
 function deletePost(id, cardElement) {
@@ -39,8 +46,53 @@ function deletePost(id, cardElement) {
       if (res.ok) {
           console.log('Ok');
           cardElement.remove();
-      }
-      
+      }      
     })
     .catch(error => console.log(error));
 }
+
+function addPost(title, descr) {
+    fetch(API, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            body: descr,
+            id: Date.now(),
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((res) => {
+            if (res.ok) {
+                console.log('Post Ok');
+                inputTitle.value = "";
+                inputDescription.value = "";
+                return res.json();
+            } else {
+              throw new ErrorEvent("Error")  
+            }
+        })
+        .then((post) => {
+            const newCardHTML = ` <div class="card" style="width: 23rem">
+          <div class="card-body">
+            <h5 class="card-title">${post.title}</h5>
+            <p class="card-text">
+              ${post.body}
+            </p>
+            <button class="btn btn-primary btn-edit" data-id="${post.id}">Edit</button>
+            <button class="btn btn-primary btn-delete" data-id="${post.id}">Delete</button>
+          </div>
+        </div>`;
+                  cards.insertAdjacentHTML('afterbegin', newCardHTML)
+    })
+    .catch(error => console.log(error));
+}
+
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const newTitle = inputTitle.value;
+    const newDescription = inputDescription.value;
+    addPost(newTitle, newDescription)
+}
+) 
