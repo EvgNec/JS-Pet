6b3b9ac4ce0c2f12bf2d437d3a57f6340abb57ae
Evgeniy <evgnec@gmail.com>
2025-01-22 20:55:40 +0200

    upd

--- src/pr03/js/fetch.js
+++ src/pr03/js/fetch.js
@@ -5,6 +5,10 @@ const inputDescription = document.querySelector('.input-description');
 const addButton = document.querySelector('.btn-add');
 const editButton = document.querySelector('.btn-edit');
 const modal = document.querySelector('.modal');
+const modalTitle = document.querySelector('.input-editTitle');
+const modalDescription = document.querySelector('.input-editDescription');
+const btnSave = document.querySelector('.btn-save');
+const btnClose = document.querySelector('.btn-close');
 
 let newCard = '';
 
@@ -41,11 +45,9 @@ cards.addEventListener('click', e => {
     addPost();
   }
   if (e.target.classList.contains('btn-edit')) {
-    console.log(e.target);
     const id = e.target.getAttribute('data-id');
-    console.log(getPostsId(id));
-
-    //  updatePost(title, description, id);
+    modal.style.display = 'block';
+    getPostsId(id);
   }
 });
 
@@ -83,7 +85,7 @@ function addPost(title, descr) {
       }
     })
     .then(post => {
-      const newCardHTML = ` <div class="card" style="width: 23rem">
+      const newCardHTML = ` <div class="card"  data-id="${post.id} style="width: 23rem">
           <div class="card-body">
             <h5 class="card-title">${post.title}</h5>
             <p class="card-text">
@@ -106,8 +108,9 @@ addButton.addEventListener('click', e => {
 });
 
 function updatePost(title, descr, id) {
-  fetch(API + '/' + id, {
-    method: 'POST',
+  const cardElement = document.querySelector(`[data-id="${id}"]`);
+  fetch(API + '/' + id, { 
+    method: 'PUT',
     body: JSON.stringify({
       title,
       body: descr,
@@ -118,18 +121,34 @@ function updatePost(title, descr, id) {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-    .then(res => {
+    .then((res)=> {
       return res.json();
     })
-    .then(updatePost => {})
-    .catch(error => console.log(error));
+    .then((updatePost) => {
+      const cardTitle = cardElement.querySelector(".card-title");
+      console.log("🚀 ~ .then ~ cardElement", cardElement)
+      const cardBody = cardElement.querySelector(".card-text");
+      cardTitle.textContent = updatePost.title;
+      cardBody.textContent = updatePost.body;
+      modal.style.display = 'none';
+      modalTitle.value = '';
+      modalDescription.value = '';
+    })
+    .catch((error) => console.log(error));
 }
+
+btnSave.addEventListener('click', e => {
+  const id = e.target.getAttribute('data-id');
+  updatePost(modalTitle.value, modalDescription.value, id);
+});
 
 function getPostsId(id) {
   fetch(API + '/' + id)
     .then(res => res.json())
     .then(data => {
-      return data;
+      modalTitle.value = data.title;
+      modalDescription.value = data.body;
+      btnSave.setAttribute('data-id', data.id);
     })
     .catch(error => console.log(error));
 }
