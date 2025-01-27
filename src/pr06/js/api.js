5872ef3b43ebd863ef924b5c91689cd82a5d7bf9
Evgeniy <evgnec@gmail.com>
2025-01-27 21:12:45 +0200

    upd

--- src/pr06/js/api.js
+++ src/pr06/js/api.js
@@ -1,10 +1,33 @@
+const cards = document.querySelector('.cards');
 
-export function getPet(){
-  axios("https://pokeapi.co/api/v2/ability/?limit=20&offset=20")
-   .then(res =>console.log(res))
+let newCard = '';
+
+export function getPet() {
+  axios('https://pokeapi.co/api/v2/ability/?limit=20&offset=100')
+    .then(res => {
+      res.data.results.map(card => {
+        return (newCard += `<div class="card" data-id="${card.url}" style="width: 20rem">
+          <div class="card-body">
+            <h5 class="card-title">${card.name}</h5>
+            <button class="btn btn-primary btn-inform" data-id="${card.url}">Information</button>
+          </div>
+        </div>`);
+      });
+      cards.innerHTML = newCard;
+    })
+
+    .catch(error => console.log(error));
 }
 
-export function getPetId(id){
-  axios(`https://pokeapi.co/api/v2/ability/${id}`)
-   .then(res =>console.log(res))
+export function getPetURL(URL) {
+  return axios(`${URL}`).then(res => {
+    const id = res.data.id;
+    return id;
+  });
+}
+
+export function getPetId(id) {
+  return  axios(`https://pokeapi.co/api/v2/pokemon/${id}`).then(res =>
+    {return res.data}
+  );
 }
--- src/pr06/js/main.js
+++ src/pr06/js/main.js
@@ -1,13 +1,42 @@
-import {getPet, getPetId} from './api.js'
+import {getPet, getPetId, getPetURL} from './api.js'
+const cards = document.querySelector('.container');
+const modal = document.querySelector('.modal');
+let informCard = '';
+getPet();
 
+cards.addEventListener('click', e => {
+    if (e.target.classList.contains('btn-inform')) {
+        const URL = e.target.getAttribute('data-id');
+        getInformation (URL);
 
-
-// getPet()
-getPetId(1);
-getPetId(2);
-getPetId(3);
-
-
-
+      modal.style.display = 'block';
+    }
+   
+  });
 
   
+async function getInformation (URL){       
+
+    let id = await getPetURL(URL);
+    let card = await getPetId(id);
+    console.log("🚀 ~ id:", card)
+
+    informCard = `<div class="card" data-id="${card.name}" style="width: 20rem">
+          <div class="card-body">
+            <h5 class="card-title">${card.name}</h5>
+    <img
+    src="${card.sprites.back_default}"
+    alt="${card.name}"
+    class="img"
+  />
+            <button class="btn btn-primary btn-close" data-id="${card.url}">Close</button>
+          </div>
+        </div>`
+
+      modal.innerHTML = informCard;
+
+}
+
+// getPetId('klutz');
+// getPetId(2);
+// getPetId(3);
