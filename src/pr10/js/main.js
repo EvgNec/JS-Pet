486b0b4b7a9d0521e11f535c57f182aa38043f1f
Evgeniy <evgnec@gmail.com>
2025-04-07 20:48:50 +0300

    upd

--- src/pr10/js/main.js
+++ src/pr10/js/main.js
@@ -10,47 +10,46 @@
 
 // import data from '../cards.json' assert { type: 'json' };
 // console.log(data);
-const jsonString=[
-    {
-        "id": 1,
-        "word_en": "want",
-        "word_ru": "хочу",
-        "word_uk": "бажаю",
-        "word_pr": "querer",
-        "word_pr_face": {
-            "Eu": "quero",
-            "Tu": "queres",
-            "Ele": "quer",
-            "Nos": "queremos",
-            "EEles": "querem"
-        }
+const jsonString = [
+  {
+    id: 1,
+    word_en: 'want',
+    word_ru: 'хочу',
+    word_uk: 'бажаю',
+    word_pr: 'querer',
+    word_pr_face: {
+      Eu: 'quero',
+      Tu: 'queres',
+      Ele: 'quer',
+      Nos: 'queremos',
+      EEles: 'querem',
     },
-    {
-        "id": 2,
-        "word_en": "can",
-        "word_ru": "хочу",
-        "word_uk": "бажаю",
-        "word_pr": "poder",
-        "word_pr_face": {
-            "Eu": "posso",
-            "Tu": "podes",
-            "Ele": "pode",
-            "Nos": "podemos",
-            "EEles": "podem"
-        }
-    }
+  },
+  {
+    id: 2,
+    word_en: 'can',
+    word_ru: 'хочу',
+    word_uk: 'бажаю',
+    word_pr: 'poder',
+    word_pr_face: {
+      Eu: 'posso',
+      Tu: 'podes',
+      Ele: 'pode',
+      Nos: 'podemos',
+      EEles: 'podem',
+    },
+  },
 ];
 
 // const data = JSON.stringify(jsonString);
 // console.log(data);
-
 
 // const jsonString2 = '[{"id": 1, "name": "John"}, {"id": 2, "name": "Alice"}, {"id": 3, "name": "Bob"}]';
 
 // const data2 = JSON.parse(jsonString2);
 // console.log(data2); // Виведе масив об'єктів
 // console.log(data2[0].name); // "John"
-
+/*
 fetch("../cards.json")
             .then(response => response.json())
             .then(data => {
@@ -60,9 +59,32 @@ fetch("../cards.json")
                 document.getElementById('synonyms').textContent = "Synonyms: " + data.word_pr;
             })
             .catch(error => console.error('Помилка завантаження JSON:', error));
+*/
+// Функція для пошуку елемента за id
+function getById(dataArray, id) {
+  return dataArray.find(item => item.id === id);
+}
+
+// Функція для відображення елемента за id
+function markById(id) {
+  fetch('../cards.json')
+    .then(response => response.json())
+    .then(jsonData => {
+      const data = getById(jsonData, 2);
+      document.getElementById('word').textContent = data.word_en;
+      document.getElementById('definition').textContent ='Definition: ' + data.word_ru;
+      document.getElementById('example').textContent = 'Example: ' + data.word_uk;
+      document.getElementById('synonyms').textContent = 'Synonyms: ' + data.word_pr;
+
+
+    })
+    .catch(error => console.error('Помилка при завантаженні JSON:', error));
+
+
+}
+
+// Функція очистки
 
 
 
-  
-  
-  
+markById(2);
