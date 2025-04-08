e4e4a66ad2f7539a3be7b2c598e33e87b23252ac
Evgeniy <evgnec@gmail.com>
2025-04-08 23:50:41 +0300

    upd

--- src/pr10/js/main.js
+++ src/pr10/js/main.js
@@ -1,27 +1,38 @@
-
 let cardId = 2;
+const buttonBlock = document.querySelector('#button-block');
+const cards = document.querySelector('.card');
 // Функція для пошуку елемента за id
 function getById(dataArray, id) {
   return dataArray.find(item => item.id === id);
 }
 
 // Функція для відображення елемента за id
-function markById(id,ln) {
+function markById(id, ln) {
   fetch('../cards.json')
     .then(response => response.json())
     .then(jsonData => {
       const data = getById(jsonData, id);
-      document.getElementById('word').textContent = data[ln].word;
+      // Очистка вмісту
+      cards.innerHTML = '';
+      handlerAdd(`${data[ln].word}`);
 
-      /*
-      document.getElementById('definition').textContent ='Definition: ' + data[ln].word;
-      document.getElementById('example').textContent = 'Example: ' + data.word_uk;
-      document.getElementById('synonyms').textContent = 'Synonyms: ' + data.word_pr;*/
+      if ((ln = 'pt')) {
+        console.log(`${[ln]}`);
+
+        handlerAdd(`'Eu: ' - ${data[ln].Eu}`);
+        handlerAdd(`'Tu: ' - ${data[ln].Tu}`);
+        handlerAdd(`'Ele: ' - ${data[ln].Ele}`);
+        handlerAdd(`'Nos: ' - ${data[ln].Nos}`);
+        handlerAdd(`'Eles: ' - ${data[ln].Eles}`);
+      }
+
+      function handlerAdd(str) {
+        const markup = `<div class="definition">${str}</div>`;
+        cards.insertAdjacentHTML('beforeend', markup);
+      }
     })
     .catch(error => console.error('Помилка при завантаженні JSON:', error));
 }
-
-const buttonBlock = document.querySelector('#button-block');
 
 buttonBlock.querySelectorAll('button').forEach(button => {
   button.addEventListener('click', () => {
@@ -31,27 +42,22 @@ buttonBlock.querySelectorAll('button').forEach(button => {
 });
 
 function handleClick(action) {
-    console.log(action);
-    switch(action) {
-      case 'pt':
-        markById(cardId,action);
-        break;
-      case 'en':
-        markById(cardId,action);
-        break;
-      case 'uk':
-        markById(cardId,action);
-        break;
-        case 'nn':
-            cardId++;
-            markById(cardId,"pt");
-            break;
-      default:
-        console.log('Невідома дія');
-    }
+  console.log(action);
+  switch (action) {
+    case 'pt':
+      markById(cardId, action);
+      break;
+    case 'en':
+      markById(cardId, action);
+      break;
+    case 'uk':
+      markById(cardId, action);
+      break;
+    case 'nn':
+      cardId++;
+      markById(cardId, 'pt');
+      break;
+    default:
+      console.log('Невідома дія');
   }
-
-
-
-
-
+}
